198999aa798731d848d01871ff206f68600899a4
JANA <janaelsaeed202gmail.com>
2025-09-07 18:56:46 +0300

    NEW!!!!

--- croquet-project/src/main.jsx
+++ croquet-project/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './index.css'   // ← this line makes your global CSS available everywhere
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
