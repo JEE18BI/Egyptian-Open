118286500604c54d12ce1d58bfd3c790177f81e0
JANA <janaelsaeed202gmail.com>
2025-09-15 16:04:39 +0300

    NEW!!!!

--- croquet-project/src/main.jsx
+++ croquet-project/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/Egyptopen">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
