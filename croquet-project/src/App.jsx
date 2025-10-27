1888c0d16a295426e4127882ea230b2109834ced
JANA <janaelsaeed202gmail.com>
2025-10-27 16:56:50 +0300

    NEW!!!!

--- croquet-project/src/App.jsx
+++ croquet-project/src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./pages/Footer.jsx";
 import SponsorBanner from "./pages/SponsorBanner";
-
+import NextYear from "./components/NextYear";
 import Home from "./pages/Home.jsx";
 import Gallery from "./pages/Gallery.jsx";
 import Players from "./pages/Players.jsx";
@@ -26,32 +26,33 @@ function App() {
 
     return (
         <div className="app-layout">
-            <Navbar />
+            <NextYear/>
+            {/*<Navbar />*/}
 
-            <main className="app-content">
-                <AnimatePresence mode="wait">
-                    <ScrollToTop />
-                    <Routes location={location} key={location.pathname}>
-                        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-                        <Route path="/egyptianopen" element={<PageWrapper><Home /></PageWrapper>} />
+            {/*<main className="app-content">*/}
+            {/*    <AnimatePresence mode="wait">*/}
+            {/*        <ScrollToTop />*/}
+            {/*        <Routes location={location} key={location.pathname}>*/}
+            {/*            <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />*/}
+            {/*            <Route path="/egyptianopen" element={<PageWrapper><Home /></PageWrapper>} />*/}
 
-                        <Route path="/clubs" element={<PageWrapper><Clubs /></PageWrapper>} />
-                        <Route path="/blocks" element={<PageWrapper><Blocks /></PageWrapper>} />
-                        <Route path="/matches" element={<PageWrapper><LiveMatches /></PageWrapper>} />
+            {/*            <Route path="/clubs" element={<PageWrapper><Clubs /></PageWrapper>} />*/}
+            {/*            <Route path="/blocks" element={<PageWrapper><Blocks /></PageWrapper>} />*/}
+            {/*            <Route path="/matches" element={<PageWrapper><LiveMatches /></PageWrapper>} />*/}
 
-                        <Route path="/announcements" element={<PageWrapper><Announcements /></PageWrapper>} />
-                        <Route path="/gallery" element={<PageWrapper><Gallery /></PageWrapper>} />
-                        <Route path="/players" element={<PageWrapper><Players /></PageWrapper>} />
-                        <Route path="/help" element={<PageWrapper><Help /></PageWrapper>} />
-                        <Route path="/tournament-details" element={<PageWrapper><TournamentDetails /></PageWrapper>} />
-                    </Routes>
-                </AnimatePresence>
-            </main>
+            {/*            <Route path="/announcements" element={<PageWrapper><Announcements /></PageWrapper>} />*/}
+            {/*            <Route path="/gallery" element={<PageWrapper><Gallery /></PageWrapper>} />*/}
+            {/*            <Route path="/players" element={<PageWrapper><Players /></PageWrapper>} />*/}
+            {/*            <Route path="/help" element={<PageWrapper><Help /></PageWrapper>} />*/}
+            {/*            <Route path="/tournament-details" element={<PageWrapper><TournamentDetails /></PageWrapper>} />*/}
+            {/*        </Routes>*/}
+            {/*    </AnimatePresence>*/}
+            {/*</main>*/}
 
             <Footer />
 
             {/* sponsor stays above footer without overlap */}
-            <SponsorBanner />
+            {/*<SponsorBanner />*/}
         </div>
     );
 }
